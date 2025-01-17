d6fe6a0043c136592f501386065343072f4e8f99
dan09143 <dstephenson27@ivytech.edu>
2025-01-16 20:19:30 -0600

    Add files via upload

---
+++ mod 1 stephenson_1:16.py
@@ -0,0 +1,5 @@
+Python 3.13.1 (v3.13.1:06714517797, Dec  3 2024, 14:00:22) [Clang 15.0.0 (clang-1500.3.9.4)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+===== RESTART: /Users/danstephenson/Documents/SVED 120/MOD 1 assingment.py =====
+It is raining cats and dogs.
